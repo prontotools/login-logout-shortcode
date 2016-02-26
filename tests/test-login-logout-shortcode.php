9d3232e0e7f96b99e0f76ef1fac490ff376cb628
zkan <kan@prontomarketing.com>
2016-02-26 14:11:30 +0700

    Initialize plugin with Dockerfile for running unit tests

---
+++ tests/test-login-logout-shortcode.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once( "login-logout-shortcode.php" );
+
+class Login_Logout_Shortcode_Test extends WP_UnitTestCase {
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function testMe() {
+    }
+}
